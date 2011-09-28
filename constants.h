33004a6afb9be9ed14edba3658c2cbdf42b83851
Andrew <andrew.c.garcia@vanderbilt.edu>
2011-09-28 02:09:14 -0500

    added global constants for offline simulator

---
+++ constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+const int FUNC_DEPTH = 12;
+const int POP_SIZE = 10;
+const int ROUNDS = 100;
+const int ROUND_START=100;
+
+const int PCC_RANGE = 20;
+const int PCD_RANGE = 100;
+const int PDD_RANGE = 50;
+#endif /*CONSTANTS_H*/
